3e8bdebe279c41cb0768f7ecf4c627daa0e822e6
Vepathakpawan <pawanved007@gmail.com>
2021-03-29 05:47:07 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InfoComponent } from './info/info.component';
+import { UserComponent } from './user/user.component'
+import { UserdetailsComponent } from './userdetails/userdetails.component'
+const routes: Routes = [
+{
+path: 'user',
+component: UserComponent
+},
+{
+path: 'userdetails',
+component: UserdetailsComponent
+},
+{
+path: 'info',
+component : InfoComponent
+}
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import { UserComponent } from './user/user.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
+import { InfoComponent } from './info/info.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserdetailsComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+      MatMenuModule,
+      MatGridListModule,
+      MatCardModule,
+      MatButtonModule,
+      MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
